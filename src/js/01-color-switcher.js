ec2b2619baa9ddce580acc3d4131bc2494447653
IhorMasechko <igor.masechko007@gmail.com>
2023-01-23 10:39:05 +0200

    push task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+let changeColor = null;
+btnStop.disabled = true;
+
+btnStart.addEventListener('click', () => {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+  changeColor = setInterval(() => {
+    let backColor = getRandomHexColor();
+    document.body.style.backgroundColor = backColor;
+  }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(changeColor);
+});
